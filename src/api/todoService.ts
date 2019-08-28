c72a676800f1532720b02f3bff8ca256352b6d6a
Andrii Denisov <andrii.denisov@yalantis.com>
2019-08-28 14:36:18 +0300

    fix redirect to todoist authorization

--- src/api/todoService.ts
+++ src/api/todoService.ts
@@ -1,7 +1,6 @@
 import TodoistApiREST from 'todoist-api-ts';
 
-const apiService = new TodoistApiREST(
-  '9ea02f65a3203b7c30a1c67f05f96c8cb5437dd7'
-);
+const apiService = new TodoistApiREST(process.env
+  .REACT_APP_USER_TOKEN as string);
 
 export default apiService;
